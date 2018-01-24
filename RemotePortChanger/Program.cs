dda0099b9efd6e50a1d328e654d4a4500810032a
Coderrob <Rob@Coderrob.com>
2018-01-23 18:18:10 -0600

    Initial commit.

---
+++ RemotePortChanger/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RemotePortChanger
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Current Remote Desktop Connection port number: " + RegistrySettings.GetRemoteDesktopPortNumber());
+
+                if (args.Length > 0)
+                {
+                    // If the remote desktop connection port number is not an integer inform the user.
+                    if (!int.TryParse(args[0], out var portNumber))
+                    {
+                        Console.WriteLine("Please provide a valid Remote Desktop Connection port number.");
+                        return;
+                    }
+
+                    RegistrySettings.SetRemoteDesktopPortNumber(portNumber);
+                    Console.WriteLine($"Updated Remote Desktop Connection port number: {RegistrySettings.GetRemoteDesktopPortNumber()}.");
+                }
+                else
+                {
+                    Console.Write("Enter a new Remote Desktop Connection port number: ");
+
+                    var input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out var portNumber))
+                    {
+                        Console.WriteLine("Please provide a valid Remote Desktop Connection port number.");
+                        return;
+                    }
+
+                    RegistrySettings.SetRemoteDesktopPortNumber(portNumber);
+
+                    Console.WriteLine($"Updated Remote Desktop Connection port number: {RegistrySettings.GetRemoteDesktopPortNumber()}.");
+                    Console.Read();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Updating Remote Desktop Connection port failed. " + ex.Message);
+            }
+        }
+    }
+}
---
+++ RemotePortChanger/RegistrySettings.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Win32;
+using NLog;
+
+namespace RemotePortChanger
+{
+    internal class RegistrySettings
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        public static RemoteDesktopSettings Settings { get; }
+
+        static RegistrySettings()
+        {
+            Settings = new RemoteDesktopSettings();
+        }
+
+        public static void SetRemoteDesktopPortNumber(int portNumber)
+        {
+            try
+            {
+                using (var key = Registry.LocalMachine.OpenSubKey(Settings.SubKey, true))
+                {
+                    if (key == null)
+                    {
+                        Logger.Warn("Unable to locate the remote desktop Tcp port number registry values.");
+                        return;
+                    }
+
+                    key.SetValue(Settings.ValueKey, portNumber);
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, $"Failed to set remote desktop Tcp port number to {portNumber}. {ex.Message}");
+            }
+        }
+
+        public static int GetRemoteDesktopPortNumber()
+        {
+            try
+            {
+                using (var key = Registry.LocalMachine.OpenSubKey(Settings.SubKey))
+                {
+                    if (key == null)
+                    {
+                        Logger.Warn("Unable to locate the remote desktop Tcp port number registry values.");
+                        return Settings.DefaultPort;
+                    }
+
+                    return Convert.ToInt32(key.GetValue(Settings.ValueKey, Settings.DefaultPort));
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, $"Failed to get remote desktop Tcp port number. {ex.Message}");
+
+                // Return default Windows RDP Tcp port number.
+                return Settings.DefaultPort;
+            }
+        }
+
+        internal class RemoteDesktopSettings
+        {
+            public int DefaultPort { get; } = 3389;
+            public string SubKey { get; } = @"System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp";
+            public string ValueKey { get; } = "PortNumber";
+        }
+    }
+}
